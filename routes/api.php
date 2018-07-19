29e41dc7d038e09cc44c3d239a70366234d3aa80
tomzcs <chaiyot@edispro.com>
2018-07-19 18:25:32 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -28,6 +28,6 @@ Route::post('register', 'API\UserController@register');
 
 Route::group(['middleware' => 'auth:api'], function(){
   Route::post('details', 'API\UserController@details');
-  Route::get('latlong', 'API\UserController@latlong');
+  Route::post('latlong', 'API\UserController@latlong');
 
 });
